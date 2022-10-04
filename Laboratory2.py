508556e2d73d910258eccb83b5839cf0f3b1c104
maksanik <maksimmor2012@yandex.ru>
2022-10-04 21:07:11 +0300

    Lab2_1

---
+++ Laboratory2.py
@@ -0,0 +1,15 @@
+import requests
+
+city = "Moscow,RU"
+appid = "02af701f09174fad401671ef150f1bd6"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+
+print("Город:", city)
+print("Погодные условия:", data['weather'][0]['description'])
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура", data['main']['temp_max'])
+
